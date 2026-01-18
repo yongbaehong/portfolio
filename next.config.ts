a9215b54325c47d0da2b3a2395f9c5c66ef5fa45
yongbaehong <yongbae40176@gmail.com>
2026-01-18 17:46:15 +0900

    setup for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export', // Required for Static Site Generation (SSG)
+  basePath: isProd ? '/yongbaehong.github.io' : '', // Replace with your repository name
+  assetPrefix: isProd ? '/yongbaehong.github.io/' : '', // Ensures CSS/JS load correctly
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support the default Next.js Image Optimization
+  },
 };
 
 export default nextConfig;
